cbc128ec65dd7c2feff29161ab68525ab9abf147
Eric Vaandering <ericvaandering@gmail.com>
2017-06-01 16:12:16 -0500

    Vastly improve the storage model and make a couple of plots. Requires pandas

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # resource-modeling
 Programs to model future CMS computing resource needs
 
-cpu.py is a python program to calculate estimates of future CMS CPU needs and expected availability.  It takes two arguments: first is the annual rate of improvement in CPU, and the second is the annual rate of improvement in software.  These get printed on the plot that is made at the end to help make sure you got them in the right order.
+`cpu.py` is a python program to calculate estimates of future CMS CPU needs and expected availability.  It takes two arguments: first is the annual rate of improvement in CPU, and the second is the annual rate of improvement in software.  These get printed on the plot that is made at the end to help make sure you got them in the right order.
+
+`disk.py` takes parameters from `BaseModel.json`, `EventCounts.json`, and another `[model].json` file as inputs
+and determines the disk and tape usage by year.
--- disk.py
+++ disk.py
@@ -2,29 +2,35 @@
 
 from __future__ import division, print_function
 
+import json
 from collections import defaultdict
+
+import pandas as pd
 
 from configure import configure
 
 PETA = 1e15
 
 model = configure('RealisticModel.json')
+YEARS = list(range(model['start_year'], model['end_year'] + 1))
+TIERS = list(model['tier_size'])
 
 # Disk space used
 dataProduced = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))  # dataProduced[year][type][tier]
 dataOnDisk = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))  # dataOnDisk[year][type][tier]
 dataOnTape = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))  # dataOnTape[year][type][tier]
+diskSamples = defaultdict(list)
+tapeSamples = defaultdict(list)
 
-# diskCopies = defaultdict(lambda: defaultdict(float))
 diskCopies = {}
 tapeCopies = {}
-for tier in model['storage_model']['versions'].keys():
+for tier in TIERS:
     diskCopies[tier] = [versions * replicas for versions, replicas in
                         zip(model['storage_model']['versions'][tier], model['storage_model']['disk_replicas'][tier])]
     tapeCopies[tier] = model['storage_model']['versions'][tier][0] * model['storage_model']['tape_replicas'][tier]
 
 # Loop over years to determine how much is produced without versions or replicas
-for year in list(range(model['start_year'], model['end_year'] + 1)):
+for year in YEARS:
     for tier, tierSize in model['tier_size'].items():
         dataProduced[year]['data'][tier] += tierSize * model['eventCounts']['data'][str(year)]
         dataProduced[year]['mc'][tier] += tierSize * model['eventCounts']['mc'][str(year)]
@@ -37,7 +43,7 @@ for year, dataDict in dataProduced.items():
             print("  %10s (%4s): %10.3f PB" % (tier, dataType, size / PETA))
 
 # Loop over years to determine how much is saved
-for year in list(range(model['start_year'], model['end_year'] + 1)):
+for year in YEARS:
     for producedYear, dataDict in dataProduced.items():
         for dataType, tierDict in dataDict.items():
             for tier, size in tierDict.items():
@@ -45,11 +51,21 @@ for year in list(range(model['start_year'], model['end_year'] + 1)):
                 tapeCopiesByDelta = tapeCopies[tier]
                 if int(producedYear) <= int(year):  # Can't save data for future years
                     if year - producedYear >= len(copiesByDelta):
-                        dataOnDisk[year][dataType][tier] += size * copiesByDelta[-1]  # Use the data for the last year
-                        dataOnTape[year][dataType][tier] += size * tapeCopiesByDelta[-1]  # Use the data for the last year
+                        diskSize = size * copiesByDelta[-1]  # Use the data for the last year
+                        tapeSize = size * tapeCopiesByDelta[-1]  # Use the data for the last year
                     else:
-                        dataOnDisk[year][dataType][tier] += size * copiesByDelta[year - producedYear]
-                        dataOnTape[year][dataType][tier] += size * tapeCopiesByDelta[year - producedYear]
+                        diskSize = size * copiesByDelta[year - producedYear]
+                        tapeSize = size * tapeCopiesByDelta[year - producedYear]
+                    if diskSize:
+                        dataOnDisk[year][dataType][tier] += diskSize
+                        diskSamples[year].append([producedYear, dataType, tier, diskSize])
+                    if tapeSize:
+                        dataOnTape[year][dataType][tier] += tapeSize
+                        tapeSamples[year].append([producedYear, dataType, tier, tapeSize])
+
+# Initialize a matrix with tiers and years
+diskByTierYear = [[0 for _i in range(len(TIERS))] for _j in YEARS]
+tapeByTierYear = [[0 for _i in range(len(TIERS))] for _j in YEARS]
 
 print("Data on disk by year and tier")
 for year, dataDict in dataOnDisk.items():
@@ -57,9 +73,16 @@ for year, dataDict in dataOnDisk.items():
     print(" %s" % year)
     for dataType, tierDict in dataDict.items():
         for tier, size in tierDict.items():
+            diskByTierYear[YEARS.index(year)][TIERS.index(tier)] += size / PETA
             yearTotal += size
             print("  %10s (%4s): %10.3f PB" % (tier, dataType, size / PETA))
-    print ("              Total: %10.3f PB" % (yearTotal / PETA))
+    print("              Total: %10.3f PB" % (yearTotal / PETA))
+
+diskFrame = pd.DataFrame(diskByTierYear, columns=TIERS, index=YEARS)
+ax = diskFrame.plot(kind='bar', stacked=True)
+ax.set(ylabel='PB on disk')
+fig = ax.get_figure()
+fig.savefig('Disk by Tier.png')
 
 print("Data on tape by year and tier")
 for year, dataDict in dataOnTape.items():
@@ -67,9 +90,21 @@ for year, dataDict in dataOnTape.items():
     print(" %s" % year)
     for dataType, tierDict in dataDict.items():
         for tier, size in tierDict.items():
+            tapeByTierYear[YEARS.index(year)][TIERS.index(tier)] += size / PETA
             yearTotal += size
             print("  %10s (%4s): %10.3f PB" % (tier, dataType, size / PETA))
-    print ("              Total: %10.3f PB" % (yearTotal / PETA))
+    print("              Total: %10.3f PB" % (yearTotal / PETA))
+
+tapeFrame = pd.DataFrame(tapeByTierYear, columns=TIERS, index=YEARS)
+ax = tapeFrame.plot(kind='bar', stacked=True)
+ax.set(ylabel='PB on tape')
+fig = ax.get_figure()
+fig.savefig('Tape by Tier.png')
+
+with open('disk_samples.json', 'w') as diskUsage, open('tape_samples.json', 'w') as tapeUsage:
+    json.dump(diskSamples, diskUsage, sort_keys=True, indent=1)
+    json.dump(tapeSamples, tapeUsage, sort_keys=True, indent=1)
+
 
 '''
 AOD:
