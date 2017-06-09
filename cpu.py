788e692638c4f0f36b160a0cf34b54438f0dec9f
Ken Bloom <kenbloom@unl.edu>
2017-06-09 15:01:49 -0500

    Implement capacity model of disk.py; not right yet?

--- cpu.py
+++ cpu.py
@@ -30,19 +30,19 @@ cpu_improvement_factor = model['improvement_factors']['hardware']
 retirement_rate = 0.05
 
 # The very important list of years
-years = list(range(model['start_year'], model['end_year']+1))
+YEARS = list(range(model['start_year'], model['end_year']+1))
 
 # Get the performance year by year which includes the software improvement factor
-reco_time = {year: performance_by_year(model, year, 'RECO', data_type='data')[0] for year in years}
+reco_time = {year: performance_by_year(model, year, 'RECO', data_type='data')[0] for year in YEARS}
 sim_time = {year: performance_by_year(model, year, 'GENSIM', data_type='mc')[0] +
                   performance_by_year(model, year, 'DIGI',data_type='mc')[0] +
-                  performance_by_year(model, year, 'RECO', data_type='mc')[0] for year in years}
+                  performance_by_year(model, year, 'RECO', data_type='mc')[0] for year in YEARS}
 
 # Running time: assume that running years (full years)
 # are all the same number of seconds, shutdown years are zero
 # 2026 is a little kludgy, it's a half year?
 
-data_seconds = {i : running_time for i in years}
+data_seconds = {i : running_time for i in YEARS}
 data_seconds[2019] = 0
 data_seconds[2020] = 0
 data_seconds[2024] = 0
@@ -51,17 +51,17 @@ data_seconds[2025] = 0
 # For completeness, an array of trigger rates, which we assume to be 1 kHz
 # until the HL-LHC starts in 2026.
 
-trigger_rate = {i : 1.0 * kilo for i in years}
+trigger_rate = {i : 1.0 * kilo for i in YEARS}
 trigger_rate[2026] = 10.0 * kilo
 trigger_rate[2027] = 10.0 * kilo
 
 # CPU time requirement calculations, in HS06 * s
 
-data_events = {i : data_seconds[i] * trigger_rate[i] for i in years}
-mc_events = {i : data_events[i] * mc_factor for i in years}
+data_events = {i : data_seconds[i] * trigger_rate[i] for i in YEARS}
+mc_events = {i : data_events[i] * mc_factor for i in YEARS}
 
-data_cpu_time = {i : data_events[i] * reco_time[i] for i in years}
-mc_cpu_time = {i : mc_events[i] * sim_time[i] for i in years}
+data_cpu_time = {i : data_events[i] * reco_time[i] for i in YEARS}
+mc_cpu_time = {i : mc_events[i] * sim_time[i] for i in YEARS}
 
 # The data need to be reconstructed about as quickly as we record them.  In
 # addition, we need to factor in express, repacking, AlCa, CAF
@@ -76,16 +76,16 @@ mc_cpu_time = {i : mc_events[i] * sim_time[i] for i in years}
 
 data_cpu_required = {i : (1.5 * data_cpu_time[i] / running_time
                               + 0.1 * data_cpu_time[i] / seconds_per_month)
-                         for i in years}
+                         for i in YEARS}
 
 # The corresponding MC, on the other hand, can be reconstructed over an
 # entire year.  We can use this to calculate the HS06 needed to do those
 # tasks.
 
-mc_cpu_required = {i : mc_cpu_time[i] / seconds_per_year for i in years}
+mc_cpu_required = {i : mc_cpu_time[i] / seconds_per_year for i in YEARS}
 
 
-total_cpu_required = {i : data_cpu_required[i] + mc_cpu_required[i] for i in years}
+total_cpu_required = {i : data_cpu_required[i] + mc_cpu_required[i] for i in YEARS}
 
 
 
@@ -108,26 +108,50 @@ with open('EventCounts.json', 'w') as eventFile:
 # for the histogram to work.  Not to mention that I couldn't get the
 # dictionary comprehension to work here.
 
-cpu_improvement = {i : cpu_improvement_factor ** (i-2017) for i in years}
+cpu_improvement = {i : cpu_improvement_factor ** (i-2017) for i in YEARS}
 
 cpu_capacity = {2016 : 1.4 * mega}
 
-for i in years:
+for i in YEARS:
     cpu_capacity[i] = cpu_capacity[i-1] * (1 - retirement_rate) + (300 if i < 2020 else 600) * kilo * cpu_improvement[i]
 del cpu_capacity[2016]    
 
-#cpu_capacity = {i : cpu_capacity[i-1] * (1 - retirement_rate) + 300 * kilo * cpu_improvement[i] for i in years}
+# CPU capacity model ala disk.py
 
-# However, in 2026, switch to a computing model in which we devote twice as
-# much of the budget to CPU, so there is a magic doubling.
+# Set the initial points
+cpuCapacity = {str(model['capacity_model']['cpu_year']): model['capacity_model']['cpu_start']}
 
-#cpu_capacity[2026] *= 2
-#cpu_capacity[2027] *= 2
+# A bit of a kludge. Assume what we have now was bought and will be retired in equal chunks over its lifetime
+cpuAdded = {}
+for year in range(model['capacity_model']['cpu_year'] - model['capacity_model']['cpu_lifetime'] + 1,
+                  model['capacity_model']['cpu_year'] + 1):
+    retired = model['capacity_model']['cpu_start'] / model['capacity_model']['cpu_lifetime']
+    cpuAdded[str(year)] = retired
 
-for i in years:
+cpuFactor = model['improvement_factors']['hardware']
+
+for year in YEARS:
+    if str(year) not in cpuCapacity:
+        cpuDelta = 0  # Find the delta which can be time dependant
+        cpuDeltas = model['capacity_model']['cpu_delta']
+        for deltaYear in sorted(cpuDeltas.keys()):
+            if int(year) >= int(deltaYear):
+                lastCpuYear = int(deltaYear)
+                cpuDelta = model['capacity_model']['cpu_delta'][deltaYear]
+                
+        cpuAdded[str(year)] = cpuDelta * cpuFactor ** (int(year) - int(lastCpuYear))
+
+        # Retire cpu added N years ago or retire 0
+
+        cpuRetired = cpuAdded.get(str(int(year) - model['capacity_model']['cpu_lifetime']), 0)
+        cpuCapacity[str(year)] = cpuCapacity[str(int(year) - 1)] + cpuAdded[str(year)] - cpuRetired
+
+
+for i in YEARS:
     print(i, '{:04.3f}'.format(data_cpu_required[i] / mega),
     '{:04.3f}'.format(mc_cpu_required[i] / mega),
-    '{:04.3f}'.format(cpu_capacity[i] / mega), 'MHS06',
+    '{:04.3f}'.format(cpu_capacity[i] / mega), 
+    '{:04.3f}'.format(cpuCapacity[str(i)] / mega), 'MHS06',
     '{:04.3f}'.format((data_cpu_required[i] + mc_cpu_required[i])/
                           (cpu_capacity[i])))
 
@@ -144,16 +168,22 @@ for year, item in sorted(mc_cpu_required.items()):
 cpuCapacityList = []
 for year, item in sorted(cpu_capacity.items()):
     cpuCapacityList.append(item/mega)
-
+altCapacityList = []
+for year, item in sorted(cpuCapacity.items()):
+    altCapacityList.append(item/mega)
+    
 # Build a data frame from lists:
 
-cpuFrame = pd.DataFrame({'Year': [str(year) for year in years],
+cpuFrame = pd.DataFrame({'Year': [str(year) for year in YEARS],
                              'Data' : cpuDataList,
                              'MC' : cpuMCList,
-                             'Capacity' : cpuCapacityList})
+                             'Capacity' : cpuCapacityList,
+                             'AltCapacity' : altCapacityList}
+                            )
 
 
 ax = cpuFrame[['Year','Capacity']].plot(x='Year',linestyle='-',marker='o', color='Red')
+cpuFrame[['Year','AltCapacity']].plot(x='Year',linestyle='-',marker='o', color='Blue',ax=ax)
 cpuFrame[['Year', 'Data', 'MC']].plot(x='Year',kind='bar',stacked=True,ax=ax)
 ax.set(ylabel='MHS06')
 ax.set(title='CPU improvement %s Software improvement = %s' %
